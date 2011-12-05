6db25a1ebbf34e4d82d1ca56ec6e6cf7d3d5b0fd
unknown <Cynthia@Cynthia-PC.(none)>
2011-12-05 10:45:08 -0500

    Added my name.

--- uno_graphics.py
+++ uno_graphics.py
@@ -1,6 +1,7 @@
 from random import randint, shuffle
 # Authors: Paul Anderson, Clayton Turner, Patrick Brewer, Scott Ziegler, Andy Stiles, Andrew Armstrong
-#Kate Harnage, Davida Mitchell, Kelsey Yetsko, Kenneth Hanson, Shane McCoy, Eric Marquesse Mathewes, Joey Randich, Jason Wilson
+#Kate Harnage, Davida Mitchell, Kelsey Yetsko, Kenneth Hanson, Shane McCoy, Eric Marquesse Mathewes, Joey Randich, Jason Wilson,
+#Cyndi Driscoll
 # Vinson Mann
 
 from graphics import *
