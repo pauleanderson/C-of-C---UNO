c16ffb3cf5ddcdb5cbf2f4d3fd30217089265fc2
Thomas <tabriggs@g.cofc.edu>
2011-12-05 11:10:04 -0500

    Changes Authors

--- uno_graphics.py
+++ uno_graphics.py
@@ -1,7 +1,7 @@
 from random import randint, shuffle
 # Authors: Paul Anderson, Clayton Turner, Patrick Brewer, Scott Ziegler, Andy Stiles, Andrew Armstrong
 #Kate Harnage, Davida Mitchell, Kelsey Yetsko, Kenneth Hanson, Shane McCoy, Eric Marquesse Mathewes, Joey Randich, Jason Wilson,
-#Cyndi Driscoll, Nelson Hazelbaker
+#Cyndi Driscoll, Thomas Briggs, Nelson Hazelbaker
 # Vinson Mann
 
 from graphics import *
